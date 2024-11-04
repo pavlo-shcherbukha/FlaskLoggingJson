4b4b47980af8232252c70b584ee32b34d1b7c32a
zz000c <pavlo.shcherbukha@pravex.ua>
2024-11-04 21:33:58 +0200

    edt fin

--- sh_app/views.py
+++ sh_app/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import time
 from operator import truediv
-from flask import Flask, render_template, request, jsonify, Response, has_request_context
+from flask import Flask, render_template, request, jsonify, Response, has_request_context, g
 from flask.logging import default_handler
 import json
 import logging
@@ -19,6 +20,45 @@ if os.environ.get("APP_DEBUG") == 'DEBUG_BRK':
     print("===========2-DEBUG-BREAK======")
 
 application = Flask(__name__)
+application.logger.removeHandler(default_handler)
+logging.getLogger('werkzeug').disabled = True
+
+@application.before_request
+def start_timer():
+    g.start = time.time()
+
+@application.after_request
+def log_request(response):
+    if request.path == '/favicon.ico':
+        return response
+
+
+    now = time.time()
+    duration = round(now - g.start, 2)
+    
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    host = request.host.split(':', 1)[0]
+    args = dict(request.args)
+
+    log_params = {
+        'method': request.method, 
+        'path': request.path,
+        'request_content_type': request.headers.get('Content-Type'),
+        'request_size': int(request.headers.get('Content-Length') or 0),
+        'status': response.status_code, 
+        'duration': duration, 
+        'time': now,
+        'ip': ip,
+        'host': host,
+        'params': args,
+        'response_content_type': response.content_type,
+        'response_size': response.calculate_content_length()
+
+    }
+
+    logger.info("HTTP API REQ", extra=log_params)
+
+    return response
 
 
 
@@ -73,7 +113,23 @@ class RequestFormatter(logging.Formatter):
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+
+apploglevel=os.environ.get("LOGLEVEL")
+if apploglevel==None:
+    logger.setLevel(logging.DEBUG)
+elif apploglevel=='DEBUG':
+    logger.setLevel(logging.DEBUG)    
+elif apploglevel=='INFO':
+    logger.setLevel(logging.INFO)    
+elif apploglevel=='WARNING':
+    logger.setLevel(logging.WARNING)    
+elif apploglevel=='ERROR':    
+    logger.setLevel(logging.ERROR)    
+elif apploglevel=='CRITICAL':
+    logger.setLevel(logging.CRITICAL)    
+else:
+    logger.setLevel(logging.DEBUG)  
+
 
 handler = logging.StreamHandler()
 handler.setFormatter(sh_app.shjsonformatter.JSONFormatter())
@@ -98,8 +154,6 @@ except Exception as e:
     ex_dsc=ex_value.args[0]
     logger.error(ex_dsc)
 
-logger.info("INFO ...... ......MESSAGE", extra={"label": "lblblblblbl"} )
-
 #=================================================
 # Головна сторінка
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -107,8 +161,8 @@ logger.info("INFO ...... ......MESSAGE", extra={"label": "lblblblblbl"} )
 @application.route("/")
 def home():
     #log("render home.html" )
-    logger=logging.getLogger(__name__).getChild("home_rout")
-    logger.info("Hoem page logger")
+    logger=logging.getLogger(__name__).getChild("home")
+    logger.debug("Home page logger")
     return render_template("home.html")
 
 
@@ -118,6 +172,8 @@ def home():
 #=================================================
 @application.route("/about/")
 def about():
+    logger=logging.getLogger(__name__).getChild("about")
+    logger.debug("About page logger")
     return render_template("about.html")
 
 
@@ -135,14 +191,16 @@ def about():
 # =================================================================================
 @application.route("/api/health", methods=["GET"])
 def health():
+    logger=logging.getLogger(__name__).getChild("health")
     title="Remote debug demo"
     label="health"
     result={}
-    log('Health check', label)
+    logger.debug('Health check')
     try:
         result= {}
         result["message"]= title
         result["ok"]=True
+        logger.debug(f"Sending response {result}")
         return json.dumps( result ), 200, {'Content-Type':'application/json'}
     except Exception as e: 
             ex_type, ex_value, ex_traceback = sys.exc_info()
@@ -158,7 +216,9 @@ def health():
             result["message"]= title
             result["error"]=ex_dsc
             result["errorCode"]=ex_name
-            result["trace"]=stack_trace 
+            result["trace"]=stack_trace
+            logger.error( ex_dsc)
+            logger.error( f"ErrorResponse: {json.dumps( result )}")
             return json.dumps( result ), 422, {'Content-Type':'application/json'}
 
 
